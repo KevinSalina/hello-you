16851acdd45bee19d3e4441a16dfe452a33be44b
Kevin Salina <kevin.salina25@gmail.com>
2021-09-13 21:23:37 -0400

    Created and styled a basic react component

---
+++ hello-you/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import TitleIntro from './components/Bio';
+
+function App() {
+  return (
+    <TitleIntro />
+  )
+}
+
+export default App;
---
+++ hello-you/src/components/Bio.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TitleIntro = () => {
+  return (
+    <div className="bio">
+      <h1 className="title">Kevin Salina</h1>
+      <p className="bio-text">I am an inspiring software developer with interests in javascript, express, and react. I also enjoy golfing, snowboarding, and punk rock music.</p>
+    </div>
+  )
+}
+
+
+export default TitleIntro;
